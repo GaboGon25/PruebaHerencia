7a029730855721e3097f75e37bf09e6a9e588f7e
CCBB-05 <CCBB-05@DESKTOP-PEQT28Q>
2023-09-07 11:37:52 -0600

    Agregar archivos de proyecto.

---
+++ PruebaHerencia/EmpleadoBaseMasComision2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaHerencia
+{
+    public class EmpleadoBaseMasComision2 : EmpleadoPorComision
+    {
+        private decimal salarioBase;
+        public EmpleadoBaseMasComision2(string nombre, string apellido, string nss, decimal tarifa, decimal venta) : base(nombre, apellido, nss, tarifa, venta)
+
+        {
+            salarioBase = salario;
+        }
+
+        public decimal SalarioBase { get { return salarioBase; } set { salarioBase = (value <0 } }
+    }
+}
---
+++ PruebaHerencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using PruebaHerencia;
+
+EmpleadoPorComision EmpleadoPorComision = new EmpleadoPorComision("Susana",
+    "Lopez", "222-22-2222", .06M, 1000.00M);
+
+Console.WriteLine("{0} {1}: \n\n{2}\n{3}: {4:C}\n",
+    "Llama a ToString de EmpleadoPorComision con referencia de"+
+    "clase base", "a objeto de clase base", EmpleadoPorComision.ToString(),
+    "ingresos", EmpleadoPorComision.Ingresos());
+
+
+
+
+
+
